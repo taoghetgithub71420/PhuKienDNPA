197d9cce78706dcb83554f8ef69931ff1aabc709
taoghetgithub71420 <cotsdow0111@gmail.com>
2021-06-27 17:21:31 +0700

    Update Controller and LayoutUer

--- PhuKienDNPA/Controllers/PhuKienController.cs
+++ PhuKienDNPA/Controllers/PhuKienController.cs
@@ -6,13 +6,12 @@ using System.Web.Mvc;
 
 namespace PhuKienDNPA.Controllers
 {
-    public class HomeController : Controller
+    public class PhuKienController : Controller
     {
+        // GET: PhuKien
         public ActionResult Index()
         {
             return View();
         }
-
-      
     }
 }
